46f051ef5eaf5f74b0df9d63b43cf53a9aa4a60f
ViktorKapustianyk <vityakap@gmail.com>
2024-08-13 18:29:44 +0300

    Moved ImageValidator initialization to the setUp() method with the @BeforeEach annotation to avoid code duplication and ensure a new instance is created before each test.

--- core/src/test/java/greencity/validator/ImageValidatorTest.java
+++ core/src/test/java/greencity/validator/ImageValidatorTest.java
@@ -1,5 +1,6 @@
 package greencity.validator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,10 +12,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ImageValidatorTest {
 
+    private ImageValidator imageValidator;
+
+    @BeforeEach
+    void setUp(){
+        imageValidator = new ImageValidator();
+    }
+
+
     @ParameterizedTest
     @ValueSource(strings = {"image/jpeg", "image/png", "image/jpg"})
     void isValidImageFileShouldContainsContentType(String contentType) {
-        ImageValidator imageValidator = new ImageValidator();
         MultipartFile multipartFile = new MockMultipartFile("file", "test.jpg", contentType, new byte[]{1, 2, 3, 4, 5});
 
         assertTrue(imageValidator.isValid(multipartFile, null));
@@ -22,10 +30,10 @@ class ImageValidatorTest {
 
     @Test
     void isValidImageFileShouldReturnTrueWhenFileIsNull() {
-        ImageValidator imageValidator = new ImageValidator();
         MultipartFile multipartFileIsNull = Mockito.mock(MultipartFile.class);
-
         Mockito.when(multipartFileIsNull.getContentType()).thenReturn(null);
+        Mockito.when(multipartFileIsNull.isEmpty()).thenReturn(true);
+
         assertFalse(imageValidator.isValid(multipartFileIsNull, null));
     }
 }
