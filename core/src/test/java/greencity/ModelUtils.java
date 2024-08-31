dc8b7057180e2e5f73eaf31070f3d9a4c08f91de
fedykvitalik2004 <90273574+fedykvitalik2004@users.noreply.github.com>
2024-08-31 16:17:57 +0300

    [74] add NewsSubscriptionControllerTest

--- core/src/test/java/greencity/ModelUtils.java
+++ core/src/test/java/greencity/ModelUtils.java
@@ -16,6 +16,7 @@ import greencity.dto.habittranslation.HabitTranslationDto;
 import greencity.dto.language.LanguageDTO;
 import greencity.dto.language.LanguageTranslationDTO;
 import greencity.dto.language.LanguageVO;
+import greencity.dto.newssubscription.NewsSubscriptionDto;
 import greencity.dto.shoppinglistitem.CustomShoppingListItemResponseDto;
 import greencity.dto.shoppinglistitem.ShoppingListItemPostDto;
 import greencity.dto.shoppinglistitem.ShoppingListItemRequestDto;
@@ -396,4 +397,9 @@ public class ModelUtils {
             .build();
     }
 
+    public static NewsSubscriptionDto getNewsSubscriptionDtoForRequest() {
+        return NewsSubscriptionDto.builder()
+            .email("email@gmail.com")
+            .build();
+    }
 }
